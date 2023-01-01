3b39f3e5c6aba88f673b003632d1db064f342bd1
kumarmanishrai <manishkumarrai79@gmail.com>
2023-01-01 22:40:14 +0530

    styles are not added

---
+++ script.js
@@ -0,0 +1,12 @@
+const joke = document.getElementById("joke")
+const btn = document.getElementById("btn")
+
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
+
+let getJoke = () => {
+    fetch(url)
+        .then(data => data.json())
+        .then(item => {joke.textContent = `${item.joke}`})
+}
+btn.addEventListener('click', getJoke)
+getJoke()
