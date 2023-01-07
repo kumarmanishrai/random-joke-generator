079fb89886c3fe818d6d5b0e8ec8a57f9acd149f
Madhur <bhartimadhur21@gmail.com>
2023-01-07 21:19:17 +0530

    Gave this project a User interface

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const joke = document.getElementById("joke")
 const btn = document.getElementById("btn")
-
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single"
 
 let getJoke = () => {
